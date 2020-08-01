10baccacb4d2b4cf65abc981ddc74a653b320487
XTroyZe <xtroyze@gmail.com>
2020-08-01 15:15:39 +0100

    Moved function into MyInfo.js and created components folder.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-// ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+import MyInfo from "./components/MyInfo.js"
 
-function MyInfo() {
-  return (
-  <div>
-    <h1>My name is Tim</h1>
-    <p>This is a test paragraph</p>
-    <ul>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-    </ul>
-  </div> 
-  )
-}
 
 ReactDOM.render(<MyInfo />,document.getElementById("root")
 );
