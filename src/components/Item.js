3bf26149e362227d307d8a7baf5a9935a0dbc3f3
XTroyZe <xtroyze@gmail.com>
2020-08-01 19:28:41 +0100

    Using javascript in a component to show the current time.

---
+++ src/components/Item.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Item() {
+  return (
+    <div>
+      <input type="checkbox" id="1" name="1"/>
+      <label for="1">To do 1</label><br></br>
+    </div>
+  )
+}
+
+export default Item
--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,14 +1,24 @@
 import React from "react"
-import Items from "./Items"
+import Item from "./Item"
 import Header from "./Header"
 
-function MyApp() {
-  return (
-    <div>
-      <Header />
-      <Items />
-    </div>
+// function MyApp() {
+//   return (
+//     <div>
+//       <Header />
+//       <Item />
+//     </div>
     
+//   )
+// }
+
+function MyApp() {
+  const date = new Date();
+  let hours = date.getHours();
+  hours = hours % 12;
+
+  return (
+    <h1>The time right now is {hours} O'Clock.</h1>
   )
 }
 
