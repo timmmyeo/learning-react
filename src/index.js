68db292c2210935bfd3be0480d0b110c96480c6a
XTroyZe <xtroyze@gmail.com>
2020-08-03 20:21:21 +0100

    Simple form that changes state

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import App from "./todo_components/App.js"
+import App from "./forms1_components/App"
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
